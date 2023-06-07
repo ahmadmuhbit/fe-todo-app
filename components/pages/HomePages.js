ba50231b039759283952da248e5bffd4d6f77c7f
ahmadmuhbit <ahmadmuhbit@gmail.com>
2023-06-07 11:02:24 +0700

    Eps 12 - Add Todo

--- components/pages/HomePages.js
+++ components/pages/HomePages.js
@@ -1,14 +1,33 @@
 import { TodoSection } from "../sections/TodoSection"
 import { TodoDoneSection } from "../sections/TodoDoneSection"
+import { AddTodoModal } from "../modals/AddTodoModal"
+import { useState } from "react"
 
 export const HomePage = () => {
+    const[openAddModal, setOpenAddModal] = useState(false)
+
+    const handleOpenModalNewTodo = () => {
+        setOpenAddModal(true)
+    }
+
     return (
-        <div className="grid grid-cols-2">
+        <div className="flex flex-col gap-2">
             <div>
-                <TodoSection />
+                <button className="rounded-lg bg-emerald-400 px-3 py-2 text-lg font-semibold" onClick={handleOpenModalNewTodo}>
+                    + add todo
+                </button>
+                {openAddModal ?
+                    <AddTodoModal handleClose={() => setOpenAddModal(false)} />
+                    : null
+                }
             </div>
-            <div>
-                <TodoDoneSection />
+            <div className="grid grid-cols-2">
+                <div>
+                    <TodoSection />
+                </div>
+                <div>
+                    <TodoDoneSection />
+                </div>
             </div>
         </div>
     )
--- service/todo/todo.service.js
+++ service/todo/todo.service.js
@@ -4,3 +4,8 @@ export const updateTodo = async (data, id) => {
     const resp = await axiosIntstance.patch(`/todos/${id}`, data)
     return resp?.data
 }
+
+export const addTodo = async (data) => {
+    const resp = await axiosIntstance.post(`/todos`, data)
+    return resp?.data
+}
